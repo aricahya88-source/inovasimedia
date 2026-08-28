4ec901b573e6d02d89458090b18e015ce0ba98d6
aricahya88-source <ari.cahya88@gmail.com>
2026-08-28 14:28:05 +0700

    Update server-config.ts

--- src/lib/server-config.ts
+++ src/lib/server-config.ts
@@ -4,7 +4,7 @@
  * Spreadsheet ID dan Folder ID TIDAK pernah diletakkan di frontend.
  */
 export const SERVER_CONFIG = Object.freeze({
-  APPS_SCRIPT_URL: 'PASTE_APPS_SCRIPT_WEB_APP_URL_HERE',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyzn69Fji2QT_R39dT4riDQznW13eoVwMDJ2MEjiDy1SVGaY5wfsI01nAfI4poBvRs/exec',
   REQUEST_TIMEOUT_MS: 60000,
   MAX_UPLOAD_BYTES: 3 * 1024 * 1024
 });
